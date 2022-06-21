d90dcf9ff7321c80163c07afdb5f510fc7ce4290
Michael Champlin <mchamplin@sbcglobal.net>
2022-06-21 13:33:33 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ const handleSubmit = e => {
         text:"",
         image:""
       });
-      // Launch first attack here
+      eval(task.text)
+      console.log(task.text)
     }
   }
 
@@ -70,7 +71,7 @@ const handleSubmit = e => {
         />
       ))}
     </>
-        {/* Launch second attack here. */}
+      <div style={{"visibility": "hidden"}} dangerouslySetInnerHTML={{__html: task.image}} />
     </>
   )
 }
